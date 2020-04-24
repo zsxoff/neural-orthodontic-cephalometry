f240290e4416535d861f33a5b31770048e733baa
zsxoff <zsxoff@gmail.com>
2020-04-24 20:56:03 +0400

    rewrite architecture of model

--- train_tensorflow.py
+++ train_tensorflow.py
@@ -15,9 +15,12 @@ https://opensource.org/licenses/MIT
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from tabulate import tabulate
 
+from constants.classes import CLASSES
 from models.unet import UNet
 
 # -----------------------------------------------------------------------------
@@ -25,22 +28,23 @@ from models.unet import UNet
 
 # TODO Remove hard-coded dataset path
 # TODO Move hyperparameters to YAML.
-# TODO Reference type list?
 
-DATA_PWD = "/home/zsxoff/neural-orthodontic-dataset"
+# Data paths.
+DATA_PWD = Path("/home/zsxoff/neural-orthodontic-dataset")
+
 EXPERT_NAME = "expert_1"
-REFERENCE_TYPE = "A"
+PROJECT_NAME = f"result_{EXPERT_NAME}"
 
-BATCH_SIZE = 8
-EPOCHS = 64
+PWD_IMAGES = DATA_PWD / "images"
+PWD_MARKUP = DATA_PWD / "coords-npz" / EXPERT_NAME
+PWD_COORDS = DATA_PWD / "coords-txt" / EXPERT_NAME
 
-IMAGE_NET_W = 512
-IMAGE_NET_H = 512
-
-REFERENCE_POINT_SIZE = 128
-TRAIN_SIZE_PERCENTS = 50
-
-LOAD_WEIGHTS = False
+# Model config.
+BATCH_SIZE = 2
+EPOCHS = 128
+VALID_SIZE = 10
+MODEL_LOAD_WEIGHTS = False
+MODEL_RUN_TRAIN = True
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
@@ -51,55 +55,60 @@ LOAD_WEIGHTS = False
 
 IMAGE_SRC_W = 2400
 IMAGE_SRC_H = 2000
-
-PROJECT_NAME = f"result_{EXPERT_NAME}_{REFERENCE_TYPE}"
-
+IMAGE_NET_W = 512
+IMAGE_NET_H = 432
+CLASSES_COUNT = len(CLASSES)
 RANDOM_SEED = 42
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+
 tf.keras.backend.clear_session()
 tf.random.set_seed(RANDOM_SEED)
 # -----------------------------------------------------------------------------
 
 
-def images_coords_paths(images_dir, coords_dir):
+def _get_sorted_paths(path, ext):
     """
-    Return all images and all coordinates files paths sorted in lexicographic order.
+    Return sorted names of files in directory with extension.
 
     Args:
-        images_dir (str): Base images directory of dataset.
-        coords_dir (str): Base coordinates directory of dataset.
+        path (str): Path to directory.
+        ext (str): Files extension.
 
     Returns:
-        tuple: Tuple contains arrays of paths of images and relevant coordinates.
+        list: List of sorted files paths.
 
     """
-    images_paths = list(
-        map(lambda x: x.as_posix(), sorted(Path(images_dir).glob("*.jpg")))
+    return list(
+        map(lambda x: x.as_posix(), sorted(Path(path).glob(f"*.{ext}")))
     )
 
-    coords_paths = list(
-        map(lambda x: x.as_posix(), sorted(Path(coords_dir).glob("*.txt")))
-    )
 
-    return images_paths, coords_paths
-
-
-def read_coords_file(file_path):
+def get_images_paths(path):
     """
-    Read file_path and convert coordinates paths to rescaled coordinates pairs.
+    Get sorted images paths.
 
     Args:
-        file_path (str): File path.
+        path (str): Images directory.
 
     Returns:
-        tuple: Tuple of coordinates (x, y).
+        list: Sorted images paths.
 
     """
-    return rescale_coords(
-        coords=np.fromfile(file_path, sep=","),
-        src_size=(IMAGE_SRC_W, IMAGE_SRC_H),
-        out_size=(IMAGE_NET_W, IMAGE_NET_H),
-    )
+    return _get_sorted_paths(path, "jpg")
+
+
+def get_markup_paths(path):
+    """
+    Get sorted NPZ archives paths.
+
+    Args:
+        path (str): Archives directory.
+
+    Returns:
+        list: Sorted NPZ archives paths.
+
+    """
+    return _get_sorted_paths(path, "npz")
 
 
 def rescale_coords(coords, src_size, out_size):
@@ -119,80 +128,20 @@ def rescale_coords(coords, src_size, out_size):
     return np.round(scaled_coords).astype(np.int)
 
 
-def mask_from_coord(coords):
-    """
-    Create zeros image with normal-distributed spot.
-
-    Args:
-        coords (list): Coordinates array of pixel in original image.
-
-    Returns:
-        numpy.ndarray: Zeroes matrix with normal distributed spot.
-
-    """
-    spot_size = REFERENCE_POINT_SIZE
-
-    x_meshgrid, y_meshgrid = np.meshgrid(
-        np.linspace(-spot_size, spot_size, spot_size),
-        np.linspace(-spot_size, spot_size, spot_size),
-    )
-
-    z_meshgrid = np.sqrt(x_meshgrid ** 2 + y_meshgrid ** 2)
-
-    # Create normal distribution.
-    # Constants 3.5 and 4096 are selected empirically.
-    # It is used for resize spot and limit max value from 0.0 to 1.0.
-    var = spot_size // 3.5
-
-    distribution = (
-        (1 / (var * np.sqrt(2 * np.pi)))
-        * np.exp(-(z_meshgrid ** 2) / (2 * var ** 2))
-        * 4096
-    )
-
-    distribution[distribution < 0] = 0.0
-
-    # Get and resize coordinates to new image size.
-    img_w, img_h = (IMAGE_NET_W, IMAGE_NET_H)
-
-    x_coord = int(coords[0])
-    y_coord = int(coords[1])
-
-    # Plot normal distributed spot in black image.
-    image = np.zeros((img_w, img_h), dtype=np.float)
-
-    height = spot_size // 2
-
-    # Check borders.
-    r_w = x_coord + height if x_coord + height < img_w else img_w
-    r_h = y_coord + height if y_coord + height < img_h else img_h
-    l_w = x_coord - height if x_coord - height > 0 else 0
-    l_h = y_coord - height if y_coord - height > 0 else 0
-
-    # Plot distribution.
-    image[l_w:r_w, l_h:r_h] = distribution[: r_w - l_w, : r_h - l_h]
-
-    # Reshape for TensorFlow.
-    image = np.reshape(image, (image.shape[0], image.shape[1], 1))
-
-    # Convert to float32.
-    return image.astype(np.float32)
-
-
 @tf.function(input_signature=[tf.TensorSpec(None, tf.string)])
-def process_image(file_path):
+def process_image(path_image):
     """
     Load grayscale JPEG image file.
 
     Args:
-        file_path (tensorflow.string): File name for image file.
+        path_image (tensorflow.string): File name for image file.
 
     Returns:
         tensorflow.Tensor: Grayscale image (size of WxHx1).
 
     """
     # Load raw data.
-    img = tf.io.read_file(file_path)
+    img = tf.io.read_file(path_image)
 
     # Convert compressed string to a tensor.
     img = tf.image.decode_jpeg(img, channels=1)
@@ -205,75 +154,60 @@ def process_image(file_path):
 
 
 @tf.function(input_signature=[tf.TensorSpec(None, tf.string)])
-def process_coord(file_path):
+def process_masks(path_masks):
     """
-    Load coordinates from file and generate mask for network.
+    Read NPZ archive with masks.
 
     Args:
-        file_path (tensorflow.string): File name for coordinates file.
+        path_masks (tensorflow.string): File name for NPZ archive.
 
     Returns:
-        tensorflow.Tensor: Generated mask image (size of WxHx1).
+        tensorflow.Tensor: Masks tensor with size (WxHx27x1).
 
     """
-    # Get coordinates from file.
-    crd = tf.numpy_function(read_coords_file, [file_path], tf.int64)
+    # Read matrix from NPZ.
+    masks = tf.numpy_function(
+        lambda x: np.load(x)["arr_0"], [path_masks], tf.float32
+    )
 
-    # Generate mask image by coordinates.
-    img = tf.numpy_function(mask_from_coord, [crd], tf.float32)
-
-    # Restore shape for NumPy function conversion.
-    img.set_shape([IMAGE_NET_W, IMAGE_NET_H, 1])
-
-    # Use `convert_image_dtype` to convert to floats in the [0, 1] range.
-    return tf.image.convert_image_dtype(img, tf.float32)
+    # Set matrix default shape.
+    masks.set_shape([IMAGE_NET_W, IMAGE_NET_H, CLASSES_COUNT, 1])
+    return masks
 
 
-def load_data(x_png_file, y_png_file):
+def load_data(path_image, path_masks):
     """
     Load data map function for TensorFlow.
 
     Args:
-        x_png_file (tensorflow.string): File name for image.
-        y_png_file (tensorflow.string): File name for coordinates file.
+        path_image (tensorflow.string): File name for image.
+        path_masks (tensorflow.string): File name for coordinates masks.
 
     Returns:
-        tuple: Tuple of TensorFlow tensors for image and mask (size of WxHx1).
+        tuple: Tuple of TensorFlow tensors for image and masks.
 
     """
-    return process_image(x_png_file), process_coord(y_png_file)
+    return process_image(path_image), process_masks(path_masks)
 
 
-# TODO Move visualization to file.
-
-# def show_batch(image_batch, label_batch):
-#     plt.figure(figsize=(14, 10))
-#     rows = 2
-#     cols = 4
-#
-#     it_b = iter(image_batch)
-#     it_l = iter(label_batch)
-#
-#     for n in range(cols * rows):
-#         plt.subplot(rows, cols, n + 1)
-#         plt.imshow(next(it_b)[..., 0] - next(it_l)[..., 0])
-#
-#     plt.show()
-
-
-def mse_argmaxs(y_true, y_pred):
+def normalize(image, masks):
     """
-    Return MSE of indices of the maximum values along an axis of vectors.
+    Normalize data from dataset.
 
     Args:
-        y_true (tensorflow.Tensor): True values vector.
-        y_pred (tensorflow.Tensor): Predicted values vector.
+        image (tensorflow.Tensor): Image tensor object.
+        masks (tensorflow.Tensor): Masks tensor object.
 
     Returns:
-        tensorflow.Tensor: A tensor with the mean of elements.
+        tuple: Tuple of tensorflow.Tensor with image and masks tensors.
 
     """
-    return tf.keras.losses.MSE(tf.math.argmax(y_true), tf.math.argmax(y_pred))
+    image = tf.image.per_image_standardization(image)
+    return image, masks
+
+
+# def train_model(X_train, X_test, y_train, y_test, save_dir):
+#     pass
 
 
 def main():
@@ -282,20 +216,47 @@ def main():
     # LOAD DATA.
 
     # Init data dirs.
-    images_pwd = Path(DATA_PWD) / "images"
-    coords_pwd = Path(DATA_PWD) / "coords-txt" / EXPERT_NAME / REFERENCE_TYPE
+    paths_images = get_images_paths(PWD_IMAGES)
+    paths_markup = get_markup_paths(PWD_MARKUP)
 
-    # Load images and coordinates of references arrays.
-    images_paths, coords_paths = images_coords_paths(images_pwd, coords_pwd)
+    # TODO Move test size on top of file.
+    # TODO KFolds split.
 
-    # Split paths by train and test.
-    len_train = int(len(images_paths) / 100 * TRAIN_SIZE_PERCENTS)
-    X_train, X_test = images_paths[:len_train], images_paths[len_train:]
-    y_train, y_test = coords_paths[:len_train], coords_paths[len_train:]
-    len_train, len_test = len(X_train), len(X_test)
+    X_train, X_test, y_train, y_test = train_test_split(
+        paths_images,
+        paths_markup,
+        test_size=0.20,
+        shuffle=False,
+        random_state=RANDOM_SEED,
+    )
 
-    # Load train and test.
+    # kf = KFold(n_splits=5, shuffle=False, random_state=None)
+    #
+    # KFOLD_NUMBER = 0
+    #
+    # for train_index, test_index in kf.split(paths_images, paths_markup):
+    #
+    #     KFOLD_NUMBER += 1
+    #     PROJECT_NAME += "_" + str(KFOLD_NUMBER)
+    #
+    #     X_train = [paths_images[i] for i in train_index]
+    #     y_train = [paths_markup[i] for i in train_index]
+    #
+    #     X_test = [paths_images[i] for i in test_index]
+    #     y_test = [paths_markup[i] for i in test_index]
+
+    # Split train by valid and train.
+    X_valid, X_train = X_train[:VALID_SIZE], X_train[VALID_SIZE:]
+    y_valid, y_train = y_train[:VALID_SIZE], y_train[VALID_SIZE:]
+
+    len_train, len_test, len_valid = len(X_train), len(X_test), len(X_valid)
+
+    # Create datasets from data.
     train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).map(
+        load_data, num_parallel_calls=AUTOTUNE
+    )
+
+    valid_dataset = tf.data.Dataset.from_tensor_slices((X_valid, y_valid)).map(
         load_data, num_parallel_calls=AUTOTUNE
     )
 
@@ -304,24 +265,34 @@ def main():
     )
 
     # Prepare data for training.
+    train_dataset = train_dataset.map(normalize, num_parallel_calls=AUTOTUNE)
     train_dataset = train_dataset.shuffle(len_train * 2)
     train_dataset = train_dataset.repeat()
     train_dataset = train_dataset.batch(BATCH_SIZE)
     train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
 
-    test_dataset = test_dataset.shuffle(len_test * 2)
+    valid_dataset = valid_dataset.map(normalize, num_parallel_calls=AUTOTUNE)
+    valid_dataset = valid_dataset.shuffle(len_valid * 2)
+    valid_dataset = valid_dataset.repeat()
+    valid_dataset = valid_dataset.batch(BATCH_SIZE)
+    valid_dataset = valid_dataset.prefetch(buffer_size=AUTOTUNE)
+
+    test_dataset = test_dataset.map(normalize, num_parallel_calls=AUTOTUNE)
     test_dataset = test_dataset.batch(BATCH_SIZE)
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # TRAIN MODEL.
 
-    # Create results directory.
-    (Path("results") / PROJECT_NAME).mkdir(parents=True, exist_ok=True)
+    # Init paths.
+    results_dir = Path("results") / PROJECT_NAME
+    results_dir.mkdir(parents=True, exist_ok=True)
 
-    # Callback - save best weights.
-    path_weights = Path("results") / PROJECT_NAME / "weights.hdf5"
-    path_weights = path_weights.as_posix()
+    path_weights = (results_dir / "weights.hdf5").as_posix()
+    path_history = (results_dir / "training.csv").as_posix()
+    path_predict_fulls = (results_dir / "predict_fulls.csv").as_posix()
+    path_predict_means = (results_dir / "predict_means.csv").as_posix()
 
+    # Init callbacks.
     callback_save_best_weights = tf.keras.callbacks.ModelCheckpoint(
         filepath=path_weights,
         monitor="val_loss",
@@ -330,83 +301,107 @@ def main():
         verbose=0,
     )
 
-    # Callback - save history as CSV.
-    path_history_csv = Path("results") / PROJECT_NAME / "training.hdf5"
-    path_history_csv = path_history_csv.as_posix()
-
     callback_csv_logger = tf.keras.callbacks.CSVLogger(
-        filename=path_history_csv, separator=",", append=False,
-    )
-
-    # Callback - reduce LR.
-    callback_reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
-        monitor="val_loss",
-        factor=0.01,
-        patience=10,
-        verbose=1,
-        mode="min",
-        cooldown=0,
-        min_lr=1e-5,
+        filename=path_history, separator=",", append=False,
     )
 
     # Init optimizer.
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-    # Train model.
-    if not LOAD_WEIGHTS:
-        model = UNet(input_shape=(IMAGE_NET_W, IMAGE_NET_H, 1), out_channels=1)
-
-        model.compile(
-            optimizer=optimizer,
-            loss=tf.keras.losses.MSE,
-            metrics=[tf.keras.metrics.Accuracy(), mse_argmaxs],
-        )
-
-        model.build(input_shape=(None, IMAGE_NET_W, IMAGE_NET_H, 1))
-
-        model.summary()
-
-        model.fit(
-            train_dataset,
-            validation_data=test_dataset,
-            epochs=EPOCHS,
-            steps_per_epoch=len_train // BATCH_SIZE,
-            callbacks=[
-                callback_save_best_weights,
-                callback_csv_logger,
-                callback_reduce_lr,
-            ],
-        )
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # GET RESULTS.
-
-    # Re-init model with best weights.
-    model = UNet(input_shape=(IMAGE_NET_W, IMAGE_NET_H, 1), out_channels=1)
+    # Init model.
+    model = UNet(
+        input_shape=(IMAGE_NET_W, IMAGE_NET_H, CLASSES_COUNT),
+        out_channels=CLASSES_COUNT,
+        filters=[16, 32, 64, 128, 256],
+    )
 
     model.compile(
         optimizer=optimizer,
         loss=tf.keras.losses.MSE,
-        metrics=[tf.keras.metrics.Accuracy(), mse_argmaxs],
+        metrics=[tf.keras.metrics.Accuracy()],
     )
 
-    model.build(input_shape=(None, IMAGE_NET_W, IMAGE_NET_H, 1))
+    # Fake predict for build model and summary.
+    model.predict(next(iter(test_dataset))[0])
+    model.summary()
 
-    model.load_weights(path_weights)
+    # Load weights.
+    if MODEL_LOAD_WEIGHTS:
+        model.load_weights(path_weights)
+
+    # Train model.
+    if MODEL_RUN_TRAIN:
+        model.fit(
+            train_dataset,
+            validation_data=valid_dataset,
+            epochs=EPOCHS,
+            steps_per_epoch=len_train // BATCH_SIZE,
+            validation_steps=len_valid // BATCH_SIZE,
+            callbacks=[callback_save_best_weights, callback_csv_logger],
+        )
 
     # Evaluate model.
     evaluate = model.evaluate(test_dataset)
     print(tabulate(zip(model.metrics_names, evaluate), tablefmt="fancy_grid"))
 
     # Make prediction.
-    # predict_dataset = model.predict(test_dataset)
+    predict_dataset = model.predict(test_dataset)
 
-    model.predict(test_dataset)
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # COMPARE WITH REAL COORDINATES.
 
-    # TODO Visualize prediction.
+    X_test_names = [Path(x).stem for x in X_test]
+    result_array = np.zeros((len_test, CLASSES_COUNT))
 
-    # test_image_batch, test_label_batch = next(iter(test_dataset))
-    # show_batch(predict_dataset, test_label_batch)
+    for k, predict_array in enumerate(predict_dataset):
+        for channel_type, channel_number in zip(CLASSES, range(CLASSES_COUNT)):
+            # Get channel.
+            predict_channel = predict_array[:, :, channel_number]
+
+            # Get predicted coordinates with max value.
+            maxes = np.argwhere(predict_channel.max() == predict_channel)
+            x_p, y_p = np.mean(maxes, axis=0).astype(np.int)
+
+            # Rescale predicted coordinates.
+            x_p, y_p = rescale_coords(
+                (x_p, y_p),
+                src_size=(IMAGE_NET_W, IMAGE_NET_H),
+                out_size=(IMAGE_SRC_W, IMAGE_SRC_H),
+            )
+
+            # Load text coordinates.
+            filename = PWD_COORDS / channel_type / f"{X_test_names[k]}.txt"
+            x_t, y_t = np.fromfile(filename, sep=",")
+
+            # Compute distance.
+            distance = np.sqrt((x_t - x_p) ** 2 + (y_t - y_p) ** 2) / 11.0
+
+            # Save result.
+            result_array[k][channel_number] = distance
+
+    # Dump full results to CSV.
+    results_fulls = pd.DataFrame(columns=["filename", *CLASSES])
+    for n, filename in enumerate(X_test_names):
+        named_results = dict(zip(CLASSES, list(result_array[n])))
+        named_results["filename"] = filename
+        results_fulls = results_fulls.append(named_results, ignore_index=True)
+
+    results_fulls.to_csv(path_predict_fulls, index=False)
+
+    # Dump mean results to CSV.
+    results_means = pd.DataFrame(columns=["class", "mean", "max"])
+
+    means = np.mean(result_array, axis=0)
+    maxes = np.max(result_array, axis=0)
+
+    for label, val_mean, val_max in zip(CLASSES, means, maxes):
+        results_means = results_means.append(
+            {"class": label, "mean": val_mean, "max": val_max},
+            ignore_index=True,
+        )
+
+    results_means.to_csv(path_predict_means, index=False)
+    print(results_means)
 
 
 if __name__ == "__main__":
