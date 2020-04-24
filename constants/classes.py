69d41fee4d327e59689bcc3ca33d9dcfd80f0dc1
zsxoff <zsxoff@gmail.com>
2020-04-24 04:58:47 +0400

    add constants package

---
+++ constants/classes.py
@@ -0,0 +1,36 @@
+"""List of classes in lexical and NPZ order."""
+
+# ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+# ! PLEASE DO NOT MODIFY THIS LIST OF CLASSES !
+# !  LIST MODIFICATION CAUSES GREAT F**K UP   !
+# ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+
+CLASSES = [
+    "A",  # 0
+    "Ar",  # 1
+    "B",  # 2
+    "Ba",  # 3
+    "C",  # 4
+    "DT pog",  # 5
+    "EN pn",  # 6
+    "Gn",  # 7
+    "Go",  # 8
+    "LL",  # 9
+    "Me",  # 10
+    "N",  # 11
+    "Or",  # 12
+    "Po",  # 13
+    "Pog",  # 14
+    "Pt",  # 15
+    "S",  # 16
+    "SNA",  # 17
+    "SNP pm",  # 18
+    "Se",  # 19
+    "Sn",  # 20
+    "UL",  # 21
+    "aii",  # 22
+    "ais",  # 23
+    "ii",  # 24
+    "is",  # 25
+    "n_",  # 26
+]
